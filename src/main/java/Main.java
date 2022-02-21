e52f86f80b072b5add51669dd40ec00ead770e3b
KainSt <ksfgnw@gmail.com>
2022-02-22 00:38:57 +0300

    sql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,26 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
+
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/world";
+
+        String url = "jdbc:mysql://localhost:3306/new_skill";
         String user = "root";
         String pass = "sqlrootpassword";
+
        try {
-           Connection connection = DriverManager.getConnection(url, user, pass);
+
+           Connection connection = DriverManager.getConnection(url,user,pass);
            Statement statement = connection.createStatement();
-           ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM country");
+           ResultSet resultSet = statement.executeQuery("SELECT name FROM Courses");
            while (resultSet.next()){
                System.out.println(resultSet.getString(1));
-               System.out.println();
-           }
+               }
            } catch(Exception ex){
-            ex.getStackTrace();
+           System.out.println("Ошибка");
+            ex.getMessage();
            }
     }
 }
