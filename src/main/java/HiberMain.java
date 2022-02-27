9f8912521c8ace40b7b34960da3c65b5390ee4a5
KainSt <ksfgnw@gmail.com>
2022-02-28 01:04:12 +0300

    sql работает/ hibernate.cfg.xml Цель задания Научиться подключать библиотеку Hibernate к проекту. Что нужно сделать •	Подключите в ваш проект библиотеку Hibernate. •	Создайте класс для таблицы Courses. •	Напишите код, который выводит имя и количество студентов любого курса.

--- src/main/java/HiberMain.java
+++ src/main/java/HiberMain.java
@@ -1,9 +1,14 @@
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class HiberMain {
     public static void main(String[] args) {
@@ -12,7 +17,20 @@ public class HiberMain {
         Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
-        Courses course = session.get(Courses.class, 1);
+
+
+
+
+        List<Courses> rows =  session.createSQLQuery("select * from courses where students_count >= 0")
+                .addEntity(Courses.class)
+                .list();
+
+        for (Courses row:rows) {
+            System.out.println(row.getName() + " - " +row.getStudentsCount() + " студентов на курсе.");
+
+        }
+
+
         System.out.println();
         sessionFactory.close();
     }
